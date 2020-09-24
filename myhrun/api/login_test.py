74223cc17aa19c74ac1c68f19b641dd859b9a67f
””liuqun“ <liuqun@51ias>
2020-09-24 13:39:49 +0800

    1.新增login用例  2.将更新tocken方法写入login用例中  3.优化当使用配置文件登录执行login用例报错时，使用debugtalk中的信息登录  4.更改部分用例2.X -》 3.X的

---
+++ myhrun/api/login_test.py
@@ -0,0 +1,42 @@
+from httprunner import HttpRunner, Config, Step, RunRequest, RunTestCase
+
+
+class TestCaseshttprunner(HttpRunner):
+
+    config = (
+        Config("login")
+        .base_url("https://b2.51ias.com")
+        )
+    teststeps = [
+        Step(
+            RunRequest("login")
+            .get("/api.php")
+            .with_params(
+                **{
+                    "password": "${ENV(password)}",
+                    "pid": "${ENV(pid)}",
+                    "type": "${ENV(type)}",
+                    "version": "${ENV(version)}",
+                    "username": "${ENV(username)}",
+                    "deviceid": "${ENV(deviceid)}",
+                    "language": "${ENV(language)}",
+                    "lz": "${ENV(lz)}",
+                    "hwdeviceid": "${ENV(hwdeviceid)}",
+                    "m": "User",
+                    "a": "login",
+                }
+            )
+            .teardown_hook("${WriteToEnv($response)}")
+            .extract()
+            .with_jmespath("content.user_info.device_info.login_token", "token")
+            .with_jmespath("body.ret", "ret")
+            .validate()
+            .assert_equal("status_code", 200)
+            .assert_equal("body.ret", 0)
+        ),
+    ]
+
+
+if __name__ == "__main__":
+    TestCaseGameInfo().test_start()
+
--- myhrun/debugtalk.py
+++ myhrun/debugtalk.py
@@ -1,5 +1,8 @@
 import requests
 import json
+import pymysql
+import os
+
 
 kw = {
         "a": "login",
@@ -9,8 +12,8 @@ kw = {
         "m": "User",
         "version": "420200722",
         "username": "22993655",
-        "deviceid": "R_iH8HGpNRzpxXbv34lcpJcyT8jxuc1S",
-        "hwdeviceid": "785cfff3-c74d-4a5f-b696-320c57fd3527"
+        "deviceid": "R_VWfhDcsKKGvRKLwNCG2DF0Jk7jKXuR",
+        "hwdeviceid": "b9d1def2-88e8-4cc3-a632-fa2f460d5625"
                 }
 def get_token():
     response = requests.get("https://b2.51ias.com/api.php",params=kw)
@@ -33,8 +36,46 @@ def get_token():
             with open('.env','w') as new_env_file:
                 for new_line in lines:
                     new_env_file.write(new_line)
-                            
     else:
-        print("login接口错误")
-if __name__ == "__main__":
-    get_token()
+        print("使用默认账号登录接口报错")
+
+def WriteToEnv(content):
+    if json.loads(content.text)["ret"]==0:
+        token= json.loads(content.text)["user_info"]["device_info"]["login_token"]
+        with open('.env','r+') as env_file:
+            lines = env_file.readlines()
+            count = 0
+            length = len(lines)
+            for line in lines:
+                count += 1
+                if count == length and line.split("=")[0] != "logintoken":
+                    env_file.write("\n"+"logintoken="+token)
+                elif  "logintoken" in line:
+                    if line.split("=")[1] != token:
+                        line = "logintoken="+token+"\n"
+                        lines[count-1] = line
+                    break
+            with open('.env','w') as new_env_file:
+                for new_line in lines:
+                    new_env_file.write(new_line)
+    else:
+        print("login登录信息错误，使用默认账号登录更新tocken")
+        get_token()
+
+def connect_mysql():
+    connect = pymysql.Connect(
+        host='rr-bp1c02r68219ep2kio.mysql.rds.aliyuncs.com',
+        port=3306,
+        user='dsyread0510',
+        passwd='PP_dhjh_234_q',
+        db = "cloudgaming",
+        charset='utf8'
+)
+    cursor = connect.cursor()
+    sql = "SELECT login_token FROM july_device where bind_account =  17307458"
+    cursor.execute(sql)
+    tocken1 = cursor.fetchall()[0][0]
+    connect.commit()
+    connect.close()
+# if __name__ == "__main__":
+#     get_token()
